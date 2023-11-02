37f62b800a62fabc728ef2fee59c69dbb2c48116
Mohamed Ben Othman <Mohamedbenothman2@outlook.com>
2023-11-02 17:59:07 +0100

    Configured the admin site

--- mysite/labelApplier/models.py
+++ mysite/labelApplier/models.py
@@ -4,3 +4,5 @@ from django.db import models
 class Label(models.Model):
     label_name = models.CharField(max_length=50)
     label_color = models.CharField(max_length=7) # hex color code
+    def __str__(self):
+        return self.label_name

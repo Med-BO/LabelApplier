03f2d01d0a20c02456bd687e7e3f92ba04b61f38
Mohamed Ben Othman <Mohamedbenothman2@outlook.com>
2023-11-03 08:59:22 +0100

    Integrated the ngx-text-annotation package

--- labelApplier_frontend/src/app/app.module.ts
+++ labelApplier_frontend/src/app/app.module.ts
@@ -11,6 +11,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule} from '@angular/forms';
 import { HttpClient, HttpClientModule, HttpHandler, HttpHeaderResponse } from '@angular/common/http';
+import { NgxAnnotateTextModule } from 'ngx-annotate-text';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { HttpClient, HttpClientModule, HttpHandler, HttpHeaderResponse } from '@
     MatInputModule,
     MatFormFieldModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxAnnotateTextModule
   ],
   providers: [HttpClient, HttpClientModule, HttpHeaderResponse],
   bootstrap: [AppComponent]
--- labelApplier_frontend/src/app/label-applier/label-applier.component.ts
+++ labelApplier_frontend/src/app/label-applier/label-applier.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { LabelServiceService } from '../services/label-service.service';
+import { Annotation, NgxAnnotateTextComponent } from 'ngx-annotate-text';
 
 @Component({
   selector: 'app-label-applier',
@@ -8,7 +9,9 @@ import { LabelServiceService } from '../services/label-service.service';
   styleUrls: ['./label-applier.component.css']
 })
 export class LabelApplierComponent implements OnInit {
-  textToLabel: string = "";
+  @ViewChild('annotateText') ngxAnnotateText?: NgxAnnotateTextComponent;
+
+  textToLabel = "";
   labels: any[] = [];
 
   constructor(
@@ -18,12 +21,51 @@ export class LabelApplierComponent implements OnInit {
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
-      this.textToLabel = params['text'];
-      console.log(this.textToLabel);
+      this.textToLabel = params['text'].toString();
     });
     this.labelService.getAllLabels().subscribe((data) => {
       this.labels = data;
       console.log(this.labels);
     });
+  }
+
+  annotations: Annotation[] = [
+    new Annotation(3, 11, 'Date', '#0069d9'),
+    new Annotation(36, 45, 'City', '#dc3545'),
+    new Annotation(47, 52, 'Country', '#28a745'),
+    new Annotation(77, 85, 'Time', '#5a6268'),
+  ];
+
+  events: string[] = [];
+
+  addAnnotation(label: string, color: string): void {
+    
+    if (!this.ngxAnnotateText) {
+      return;
+    }
+
+    const selection = this.ngxAnnotateText.getCurrentTextSelection();
+    console.log(selection);
+    if (!selection) {
+      console.log("test");
+      return;
+    }
+
+    if (this.ngxAnnotateText.isOverlappingWithExistingAnnotations(selection)) {
+      alert('The selected text is already annotated.');
+      return;
+    }
+
+    const annotation = new Annotation(selection.startIndex, selection.endIndex, label, color);
+    this.annotations = this.annotations.concat(annotation);
+    this.events.push(`Added '${annotation}'`);
+  }
+
+  onClickAnnotation(annotation: Annotation) {
+    this.events.push(`Clicked on '${annotation}'`);
+  }
+
+  onRemoveAnnotation(annotation: Annotation): void {
+    this.events.push(`Removed '${annotation}'`);
   }
 }

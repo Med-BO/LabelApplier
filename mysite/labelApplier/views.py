8d35d397914c36be0216c7471b708ef51d6fba14
Mohamed Ben Othman <Mohamedbenothman2@outlook.com>
2023-11-03 06:57:13 +0100

    Integrated the REST framework

--- mysite/labelApplier/views.py
+++ mysite/labelApplier/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("label applier homepage")
+@api_view(['GET'])
+def getData(request):
+    return Response()
